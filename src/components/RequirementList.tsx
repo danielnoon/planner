7caf4d7abe220067276b943c412458339dd132a6
Daniel Noon <supercookies@outlook.com>
2021-07-21 21:03:12 -0600

    clean up some SEO/presentation stuff

--- src/components/RequirementList.tsx
+++ src/components/RequirementList.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 import Requirement from "../types/Requirement";
 import Card from "./primitives/Card";
+import Center from "./primitives/Center";
 import Grid from "./primitives/Grid";
 import { Unordered } from "./primitives/List";
 import ListItem from "./primitives/ListItem";
+import Padding from "./primitives/Padding";
 
 interface RequirementListProps {
   requirements: Requirement[];
@@ -11,19 +13,40 @@ interface RequirementListProps {
 }
 
 const RequirementList: FC<RequirementListProps> = ({ requirements }) => {
-  return <Card>
-    <Unordered>
-      {
-        requirements.map(r => 
-          <ListItem key={r.id}>
-            <Grid templateColumns="1fr">
-              <span>{r.name}</span>
-            </Grid>
-          </ListItem>
-        )
-      }
-    </Unordered>
-  </Card>
-}
+  // return <Card>
+  //   <Unordered>
+  //     {
+  //       requirements.map(r =>
+  //         <ListItem key={r.id}>
+  //           <Grid templateColumns="1fr">
+  //             <span>{r.name}</span>
+  //           </Grid>
+  //         </ListItem>
+  //       )
+  //     }
+  //   </Unordered>
+  // </Card>
+
+  return (
+    <Card>
+      <Padding>
+        <Center>
+          <h3 style={{ textAlign: "center" }}>
+            Requirements have not been implemented yet!
+          </h3>
+          <p
+            style={{
+              fontSize: 14,
+              textAlign: "center",
+              color: `rgba(0, 0, 0, 0.8)`,
+            }}
+          >
+            For now, you can get started by adding your classes.
+          </p>
+        </Center>
+      </Padding>
+    </Card>
+  );
+};
 
 export default RequirementList;
